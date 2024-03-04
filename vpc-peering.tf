bb53121f013bc86a27acf9c722159baac03b9f73
shashankaitha <shashankaitha8@gmail.com>
2024-03-04 09:05:23 +0530

    Updated_peering

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -1,38 +1,38 @@
-# data "aws_vpc" "ansible_vpc" {
-#   id = "vpc-0eac6d4d55868caa8"
-# }
+data "aws_vpc" "ansible_vpc" {
+  id = "vpc-07ba286befca1ed30"
+}
 
-# data "aws_route_table" "ansible_vpc_rt" {
-#   subnet_id = "subnet-0d124b5eb12011584"
-# }
+data "aws_route_table" "ansible_vpc_rt" {
+  subnet_id = "subnet-0c1253b5d27eccc55"
+}
 
-# resource "aws_vpc_peering_connection" "foo" {
-#   peer_vpc_id = data.aws_vpc.ansible_vpc.id
-#   vpc_id      = aws_vpc.default.id
-#   auto_accept = true
-#   accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+resource "aws_vpc_peering_connection" "foo" {
+  peer_vpc_id = data.aws_vpc.ansible_vpc.id
+  vpc_id      = aws_vpc.default.id
+  auto_accept = true
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   tags = {
-#     Name = "Ansible-${var.vpc_name}-Peering"
-#   }
-# }
+  tags = {
+    Name = "Ansible-${var.vpc_name}-Peering"
+  }
+}
 
-# resource "aws_route" "peering-to-ansible-vpc" {
-#   route_table_id            = aws_route_table.terraform-public.id
-#   destination_cidr_block    = "10.40.0.0/16"
-#   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
-#   #depends_on                = [aws_route_table.terraform-public]
-# }
+resource "aws_route" "peering-to-ansible-vpc" {
+  route_table_id            = aws_route_table.terraform-public.id
+  destination_cidr_block    = "10.40.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+  #depends_on                = [aws_route_table.terraform-public]
+}
 
-# resource "aws_route" "peering-from-ansible-vpc" {
-#   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-#   destination_cidr_block    = "10.36.0.0/16"
-#   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
-#   #depends_on                = [aws_route_table.terraform-public]
-# }
+resource "aws_route" "peering-from-ansible-vpc" {
+  route_table_id            = data.aws_route_table.ansible_vpc_rt.id
+  destination_cidr_block    = "10.36.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+  #depends_on                = [aws_route_table.terraform-public]
+}
