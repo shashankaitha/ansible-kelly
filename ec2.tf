13db17040f34d1a31e125f6b9c7e9f91af502dad
shashankaitha <shashankaitha8@gmail.com>
2024-03-04 08:54:07 +0530

    Removed peering and data

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-data "aws_ami" "my_ami" {
-  most_recent = true
-  name_regex  = "^DevSecOps"
-  owners      = ["721834156908"]
-}
+# data "aws_ami" "my_ami" {
+#   most_recent = true
+#   name_regex  = "^DevSecOps"
+#   owners      = ["721834156908"]
+# }
 
 
 resource "aws_instance" "webservers" {
   count                       = local.new_environment == "production" ? 3 : 1
-  ami                         = data.aws_ami.my_ami.id
+  ami                         = "ami-0cd59ecaf368e5ccf"
   instance_type               = lookup(var.instance_type, local.new_environment)
   key_name                    = var.key_name
   subnet_id                   = element(aws_subnet.public-subnets.*.id, count.index)
@@ -18,7 +18,7 @@ resource "aws_instance" "webservers" {
     Terraform-Managed = "Yes"
     Env               = local.new_environment
     ProjectID         = local.projid
-    ManagedBy = "Terraform"
+    ManagedBy         = "Terraform"
   }
 }
 
